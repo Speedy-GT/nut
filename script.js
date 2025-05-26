d9526933829fbbdb241b7378f721a7d961223fd8
Speedy-GT <notshadowgt@gmail.com>
2025-05-25 21:03:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,3 @@ function playSong(path) {
   player.src = path;
   player.play();
 }
-
-function goToPhonk() {
-  // You can link to a different HTML page here if needed
-  alert("Redirecting to Phonk page...");
-}
